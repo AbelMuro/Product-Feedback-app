0ea80293d503665d98400ac1174ad1e1ae5d0fac
abelmuro <abelmuro93@gmail.com>
2023-03-14 11:38:57 -0700

    commit

--- src/Components/App/SuggestionsPage/LogoBox/LogoBox.js
+++ src/Components/App/SuggestionsPage/LogoBox/LogoBox.js
@@ -1,13 +1,15 @@
-import React, {useState, useCallback, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import styles from './styles.module.css';
 import images from './images';
 
 
-/* now i will need to create a blank container that will float to the right, this will server as the
- mobile menu, 
-*/
+/* keep in mind that this component will turn into a mobile menu bar*/
+
+/* TODO: make the filter and the roadmap component re-usable*/
 function LogoBox() {
     const [showMobileMenu, setShowMobileMenu] = useState(false)
+    const mobileMenuRef = useRef()
+
 
     const handleClick = () => {
         setShowMobileMenu(!showMobileMenu);
@@ -16,7 +18,6 @@ function LogoBox() {
 
     useEffect(() => {
         const mobileIcon = document.querySelector('.' + styles.mobileIcon)
-        console.log(mobileIcon.style);
 
         if(showMobileMenu){
             mobileIcon.style.maskImage = `url(${images['closeIcon']}`;
@@ -28,18 +29,42 @@ function LogoBox() {
         }
     }, [showMobileMenu])
 
+    useEffect(() => {
+        if(showMobileMenu){
+            mobileMenuRef.current.style.display = 'block'
+            setTimeout(() => {
+                mobileMenuRef.current.style.opacity = '1';
+            }, 100)
+        }
+        else{
+            mobileMenuRef.current.style.opacity = ''; 
+            setTimeout(() => {
+                mobileMenuRef.current.style.display = '';
+            }, 200)
+        }
+
+            
+    }, [showMobileMenu])
+
     return(
-        <section className={styles.container}>
-            <div className={styles.content}>
-                <h2 className={styles.title}>
-                    Frontend Mentor
-                </h2>
-                <p className={styles.desc}>
-                    Feedback Board
-                </p>
-            </div>
-            <div className={styles.mobileIcon} onClick={handleClick}></div>
-        </section>
+        <>
+            <section className={styles.container}>
+                <div className={styles.content}>
+                    <h2 className={styles.title}>
+                        Frontend Mentor
+                    </h2>
+                    <p className={styles.desc}>
+                        Feedback Board
+                    </p>
+                </div>
+                <div className={styles.mobileIcon} onClick={handleClick}></div>
+            </section>     
+            <section className={styles.overlay} ref={mobileMenuRef}>
+                <div className={styles.mobileMenu}></div>
+            </section>   
+            
+        </>
+
     )
 }
 
