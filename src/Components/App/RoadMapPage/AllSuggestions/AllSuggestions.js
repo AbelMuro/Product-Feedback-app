ca5ddaa7733e4ef7ee10a014a35ccdca78e4d958
abelmuro <abelmuro93@gmail.com>
2023-03-22 13:35:05 -0700

    commit

---
+++ src/Components/App/RoadMapPage/AllSuggestions/AllSuggestions.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+function AllSuggestions() {
+    return(
+        <section className={styles.container}>
+
+        </section>
+    )
+}
+
+export default AllSuggestions
---
+++ src/Components/App/RoadMapPage/AllSuggestions/index.js
@@ -0,0 +1,3 @@
+import AllSuggestions from "./AllSuggestions.js";
+
+export default AllSuggestions;
